ce81a36b40939594449539303fcf9826192ca7a1
SrMetus <s.gaggero87@gmail.com>
2024-01-09 20:53:58 -0300

    add completed todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { Todos } from "./componets/Todos"
+import { type TodoId, type Todo as TodoType } from "./types"
 
 const mockTodos = [
   { 
@@ -22,8 +23,22 @@ const mockTodos = [
 const App = () => {
   const [todos, setTodos] = useState(mockTodos)
 
-  const handleRemove = (id: string): void => {
+  const handleRemove = ({id}: TodoId): void => {
     const newTodos = todos.filter(todo => todo.id != id)
+    setTodos(newTodos)
+  }
+
+  const HandleCompleted = (
+    { id, completed }: Pick<TodoType, 'id' | 'completed'>
+    ):void => {
+      const newTodos = todos.map(todo =>{
+        if(todo.id === id){
+          return {
+            ...todo, completed
+          }
+      }
+      return todo
+    })
     setTodos(newTodos)
   }
 
@@ -31,6 +46,7 @@ const App = () => {
     <>
     <div className="todoapp">
       <Todos
+        onToggleCompleteTodo={HandleCompleted}
         onRemoveTodo={handleRemove} 
         todos = { todos } />
     </div>
--- src/componets/Todos.tsx
+++ src/componets/Todos.tsx
@@ -1,12 +1,13 @@
-import { type ListOfTodos } from "../types"
+import { type Todo as TodoType, TodoId, type ListOfTodos } from "../types"
 import { Todo } from "./todo"
 
 interface Props {
     todos: ListOfTodos
-    onRemoveTodo: (id: string) => void
+    onToggleCompleteTodo: ({id, completed}: Pick<TodoType, 'id' | 'completed'>) => void
+    onRemoveTodo: ({id}: TodoId) => void
 }
 
-export const Todos: React.FC<Props> = ({ todos, onRemoveTodo }) => {
+export const Todos: React.FC<Props> = ({ todos, onRemoveTodo, onToggleCompleteTodo }) => {
   return (
     <ul className="todo-list">
         {todos.map(todo => (
@@ -18,6 +19,7 @@ export const Todos: React.FC<Props> = ({ todos, onRemoveTodo }) => {
                     id={todo.id}
                     title={todo.title}
                     completed={todo.completed}
+                    onToggleCompleteTodo={onToggleCompleteTodo}
                     onRemoveTodo={onRemoveTodo}
                 />
             </li>
