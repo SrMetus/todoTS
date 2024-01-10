a4f43ed389c1cd99affc0fbb8df8384b7de58285
SrMetus <s.gaggero87@gmail.com>
2024-01-09 23:28:02 -0300

    todo completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react"
 import { Todos } from "./componets/Todos"
-import { type TodoId, type Todo as TodoType } from "./types"
+import { FilterValue, TodoTitle, type TodoId, type Todo as TodoType } from "./types"
+import { ALL_FILTERS } from "./consts"
+import { Footer } from "./componets/Footer"
+import { Header } from "./componets/Header"
 
 const mockTodos = [
   { 
     id: '1',
-    title: 'Reforsar javaScript',
+    title: 'Reforzar JavaScript',
     completed: false
   },
   {
@@ -22,13 +25,14 @@ const mockTodos = [
 
 const App = () => {
   const [todos, setTodos] = useState(mockTodos)
+  const [filterSelected, setFilterSelected] = useState<FilterValue>(ALL_FILTERS)
 
   const handleRemove = ({id}: TodoId): void => {
     const newTodos = todos.filter(todo => todo.id != id)
     setTodos(newTodos)
   }
 
-  const HandleCompleted = (
+  const handleCompleted = (
     { id, completed }: Pick<TodoType, 'id' | 'completed'>
     ):void => {
       const newTodos = todos.map(todo =>{
@@ -42,13 +46,51 @@ const App = () => {
     setTodos(newTodos)
   }
 
+  const handleFilterChange = (filter: FilterValue): void => {
+    setFilterSelected(filter)
+  }
+
+  const handleRemoveAllCompleted = (): void => {
+    const newTodos = todos.filter(todo => !todo.completed)
+    setTodos(newTodos)
+  } 
+
+  const activeCount = todos.filter(todo => !todo.completed).length
+  const completedCount = todos.length - activeCount
+
+  const filteredTodos = todos.filter(todo => {
+    if(filterSelected === ALL_FILTERS.ACTIVE) return !todo.completed
+    if(filterSelected === ALL_FILTERS.COMPLETED) return todo.completed
+    return todo
+  })
+
+  const handleAddTodo = ({ title }: TodoTitle): void => {
+    const newTodo = {
+      title,
+      id: crypto.randomUUID(),
+      completed: false
+    }
+
+    const newTodos = [ ...todos, newTodo]
+    setTodos(newTodos)
+  }
+
   return (
     <>
     <div className="todoapp">
+      <Header onAddTodo={handleAddTodo}/>
       <Todos
-        onToggleCompleteTodo={HandleCompleted}
+        onToggleCompleteTodo={handleCompleted}
         onRemoveTodo={handleRemove} 
-        todos = { todos } />
+        todos = { filteredTodos }
+      />
+      <Footer
+        activeCount={activeCount}
+        completedCount={completedCount}
+        filterSelected= { filterSelected }
+        onClearCompleted={handleRemoveAllCompleted}
+        handleFilterChange={handleFilterChange}
+      />
     </div>
     </>
   )
